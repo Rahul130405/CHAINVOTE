7fb3c844cd935f1dc6a6f2f3a47e0ab7bb413aa4
Rahul Raj Jaiswal <yashckp@gmail.com>
2026-07-26 14:29:52 +0530

    fix: add favicon.ico route to handle 404

--- chainvote/urls.py
+++ chainvote/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('voting.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'voting/images/favicon.ico')),
 ]
