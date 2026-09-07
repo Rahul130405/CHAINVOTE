cb461668b3ae56e6160d6470431c7f14d44e3534
Rahul Raj Jaiswal <yashckp@gmail.com>
2026-09-07 15:33:53 +0530

    Use CompressedStaticFilesStorage with manifest strict False to prevent 500 on static files

--- chainvote/settings.py
+++ chainvote/settings.py
@@ -85,7 +85,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
